5ad1ad596d6db223780ec07fe645245c8ac099c6
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 13:57:46 +0800

    迁移：按 ERP 还原设置新密码流程

--- src/views/settingNewPassword.vue
+++ src/views/settingNewPassword.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="login_container">
-    <div class="login-form">
+  <div
+    :class="[
+      'login_container',
+      isMobile ? 'login_container-mobile' : '',
+      active === '0' ? 'active-bgc' : ''
+    ]"
+  >
+    <div v-if="active === '1'" class="login-form">
       <div class="title">Setting New Password</div>
-      <el-form label-width="140px" ref="form" :model="form" :rules="rules">
+      <el-form
+        ref="form"
+        :model="form"
+        :rules="rules"
+        label-width="140px"
+        :label-position="isMobile ? 'top' : 'right'"
+      >
         <el-form-item
           label="New Password"
           prop="password"
@@ -10,29 +22,32 @@
         >
           <el-input
             v-model="form.password"
-            autoComplete="off"
             type="password"
+            autocomplete="off"
+            clearable
             show-password
+            @input="removeWhitespace('password', $event)"
           >
-            <template v-slot:prefix>
+            <template #prefix>
               <svg-icon
-                style="height: 22px"
                 icon-class="password"
                 class="el-input__icon input-icon"
               />
             </template>
           </el-input>
         </el-form-item>
+
         <el-form-item label="Confirm Password" prop="confirmPassword">
           <el-input
             v-model="form.confirmPassword"
-            autoComplete="off"
             type="password"
+            autocomplete="off"
+            clearable
             show-password
+            @input="removeWhitespace('confirmPassword', $event)"
           >
-            <template v-slot:prefix>
+            <template #prefix>
               <svg-icon
-                style="height: 22px"
                 icon-class="password"
                 class="el-input__icon input-icon"
               />
@@ -40,6 +55,7 @@
           </el-input>
         </el-form-item>
       </el-form>
+
       <div style="width: 100%; margin: 40px 0 25px; text-align: center">
         <el-button
           type="primary"
@@ -51,6 +67,10 @@
         </el-button>
       </div>
     </div>
+
+    <div v-if="active === '0'" class="link-expired">
+      The link has expired.
+    </div>
   </div>
 </template>
 
@@ -60,8 +80,10 @@ import {
   setNewPasswordInfo,
   setInitialPassword,
   resetPasswordForSubmitVC,
-  resetPasswordForSubmitPW
+  resetPasswordForSubmitPW,
+  resetPasswordForVerificationUuid
 } from '@/api/login'
+
 export default {
   name: 'TmsWebSettingnewpassword',
   data() {
@@ -72,13 +94,15 @@ export default {
         password: '',
         confirmPassword: '',
         passwordType: false,
-        newPasswordType: false
+        newPasswordType: false,
+        userUuid: '',
+        verificationCode: '',
+        resetPasswordUuid: ''
       },
       rules: {
         password: [
           {
             required: true,
-
             pattern: new RegExp(/^(?!(\s+$))/g),
             trigger: ['blur', 'change'],
             message: 'New Password is required.'
@@ -93,7 +117,6 @@ export default {
         confirmPassword: [
           {
             required: true,
-
             pattern: new RegExp(/^(?!(\s+$))/g),
             trigger: ['blur', 'change'],
             message: 'Confirm Password is required.'
@@ -107,42 +130,51 @@ export default {
         ]
       },
       isLoading: false,
-      t: ''
+      t: '',
+      active: undefined
+    }
+  },
+  computed: {
+    isMobile() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
     }
   },
   created() {
-    this.form = {
-      registerId: '',
-      uuid: '',
-      password: '',
-      confirmPassword: '',
-      passwordType: false,
-      newPasswordType: false,
-      userUuid: '',
-      verificationCode: '',
-      resetPasswordUuid: ''
-    }
     this.form.registerId = this.$route.query.id || ''
     this.form.uuid = this.$route.query.uuid || ''
     this.form.userUuid = this.$route.query.userUuid || ''
     this.form.verificationCode = this.$route.query.verificationCode || ''
     this.t = this.$route.query.t || ''
-    if (this.form.registerId && this.form.uuid) {
-      this.setNewPasswordInfo()
-    } else if (this.form.userUuid && this.form.verificationCode) {
-      this.resetPasswordForSubmitVC()
-    }
+    this.verifyResetPasswordLink()
   },
-  activated() {},
   methods: {
+    removeWhitespace(field, value) {
+      this.form[field] = value.replace(/\s/g, '')
+    },
+    clearInput(field) {
+      this.form[field] = undefined
+    },
+    verifyResetPasswordLink() {
+      return resetPasswordForVerificationUuid({
+        userUuid: this.form.userUuid
+      })
+        .then(res => {
+          this.active = res.msg
+          if (res.msg === '1') {
+            return this.submitVerificationCode()
+          }
+          return undefined
+        })
+        .catch(() => {})
+    },
     setNewPasswordInfo() {
-      setNewPasswordInfo({
+      return setNewPasswordInfo({
         registerId: this.form.registerId,
         uuid: this.form.uuid
       })
         .then(res => {
-          console.log(res)
-          // active: 链接是否有效： 0否, 1是
           if (res.data.active === 0) {
             if (res.data.registerType === '1') {
               sessionStorage.setItem('entryIdx', 2)
@@ -154,8 +186,8 @@ export default {
         })
         .catch(() => {})
     },
-    resetPasswordForSubmitVC() {
-      resetPasswordForSubmitVC({
+    submitVerificationCode() {
+      return resetPasswordForSubmitVC({
         userUuid: this.form.userUuid,
         verificationCode: this.form.verificationCode
       })
@@ -164,100 +196,93 @@ export default {
         })
         .catch(() => {})
     },
-    changeInputType(type) {
-      this.form[type] = !this.form[type]
-    },
     submit() {
       if (this.form.password && this.form.confirmPassword) {
         this.form.password = this.form.password.trim()
         this.form.confirmPassword = this.form.confirmPassword.trim()
       }
+
       this.$refs.form
         .validate()
         .then(valid => {
-          if (valid) {
-            if (this.form.password !== this.form.confirmPassword) {
-              this.$modal.msgError(
-                'The passwords entered twice are inconsistent.'
-              )
-              return
-            }
-            this.isLoading = true
-            if (this.form.userUuid && this.form.verificationCode) {
-              resetPasswordForSubmitPW({
-                password: this.form.password,
-                resetPasswordUuid: this.form.resetPasswordUuid,
-                userUuid: this.form.userUuid
-              })
-                .then(res => {
-                  // 登录页
-                  sessionStorage.setItem('type', '')
-                  this.$router.push({
-                    path: '/login',
-                    query: { redirect: '/index' }
-                  })
-                  this.$modal.msgSuccess(this.$t('login.resetSuccess'))
-                })
-                .catch(err => {
-                  console.log(err)
-                })
-              return
-            }
-            const { passwordType, newPasswordType, ...params } = this.form
-            if (this.t === '1') {
-              setNewPassword(params).then(res => {
+          if (!valid) return
+
+          if (this.form.password !== this.form.confirmPassword) {
+            this.$modal.msgError(
+              'The passwords entered twice are inconsistent.'
+            )
+            return
+          }
+
+          this.isLoading = true
+          if (this.form.userUuid && this.form.verificationCode) {
+            this.submitResetPassword()
+            return
+          }
+
+          const { passwordType, newPasswordType, ...params } = this.form
+          if (this.t === '1') {
+            setNewPassword(params)
+              .then(() => {
                 sessionStorage.setItem('entryIdx', 2)
                 this.$router.push({ path: '/' }).catch(() => {})
+              })
+              .finally(() => {
                 this.isLoading = false
               })
-              return
-            }
-            setInitialPassword(params)
-              .then(res => {
-                const query = res.data
-                query.username = res.data.account
-                query.tenantType = res.data.registerType
-                if (res.data.registerType === '1') {
-                  sessionStorage.setItem('entryIdx', 2)
-                  query.tenantCode = 'Customer'
-                } else {
-                  sessionStorage.setItem('entryIdx', 1)
-                }
-                // 运营平台
-                /**
-                    code: "10"
-                    password: "123456"
-                    tenantType: 3
-                    username: "admin"
-                    uuid: "72a657bfef2f4760a9586edb78bc995c"
-                 */
-
-                // 承运商
-                /**
-                    code: "13"
-                    password: "123456"
-                    tenantCode: "OceanCarrier"
-                    tenantType: 2
-                    username: "OceanCarrier"
-                    uuid: "7240f049915541d68f4220f5324444c9"
-                 */
-                this.$store.dispatch('FedLogOut')
-                this.$store
-                  .dispatch('Login', query)
-                  .then(() => {
-                    this.$router.push({ path: '/' }).catch(() => {})
-                    this.isLoading = false
-                  })
-                  .catch(() => {
-                    this.isLoading = false
-                  })
-              })
-              .catch(() => {
-                this.isLoading = false
-              })
+            return
           }
+
+          setInitialPassword(params)
+            .then(res => {
+              const query = res.data
+              query.username = res.data.account
+              query.tenantType = res.data.registerType
+              if (res.data.registerType === '1') {
+                sessionStorage.setItem('entryIdx', 2)
+                query.tenantCode = 'Customer'
+              } else {
+                sessionStorage.setItem('entryIdx', 1)
+              }
+
+              this.$store.dispatch('FedLogOut')
+              return this.$store.dispatch('Login', query)
+            })
+            .then(() => {
+              this.$router.push({ path: '/' }).catch(() => {})
+            })
+            .finally(() => {
+              this.isLoading = false
+            })
         })
         .catch(() => {})
+    },
+    submitResetPassword() {
+      resetPasswordForVerificationUuid({ userUuid: this.form.userUuid })
+        .then(res => {
+          this.active = res.msg
+          if (res.msg !== '1') {
+            this.isLoading = false
+            return
+          }
+
+          return resetPasswordForSubmitPW({
+            password: this.form.password,
+            resetPasswordUuid: this.form.resetPasswordUuid,
+            userUuid: this.form.userUuid
+          }).then(() => {
+            sessionStorage.setItem('type', '')
+            this.$router.push({
+              path: '/login',
+              query: { redirect: '/index' }
+            })
+            this.$modal.msgSuccess(this.$t('login.resetSuccess'))
+            this.isLoading = false
+          })
+        })
+        .catch(() => {
+          this.isLoading = false
+        })
     }
   }
 }
@@ -265,12 +290,13 @@ export default {
 
 <style lang="scss" scoped>
 .title {
-  margin: 0px auto 30px auto;
+  margin: 0 auto 30px auto;
   font-size: 18px;
   font-weight: 700;
   text-align: center;
   color: #1890ff;
 }
+
 .login_container {
   display: flex;
   flex-direction: column;
@@ -280,13 +306,56 @@ export default {
   height: 100vh;
   min-height: 800px;
   background: linear-gradient(90deg, #40c2ff, #4084ff);
+
+  .login-form {
+    position: relative;
+    border-radius: 6px;
+    background: #ffffff;
+    width: 500px;
+    padding: 25px 25px 5px 25px;
+  }
 }
 
-.login-form {
-  position: relative;
-  border-radius: 6px;
-  background: #ffffff;
-  width: 500px;
-  padding: 25px 25px 5px 25px;
+.link-expired {
+  font-size: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.login_container-mobile {
+  position: fixed;
+  top: 0;
+  left: 0;
+  min-width: 0;
+  min-height: 0;
+  width: 100vw;
+  height: 100vh;
+
+  .login-form {
+    position: relative;
+    border-radius: 6px;
+    background: #ffffff;
+    width: 90vw;
+    max-width: 500px;
+    padding: 20px;
+
+    :deep(.el-form-item) {
+      margin-bottom: 10px !important;
+    }
+
+    :deep(.el-form-item__label) {
+      padding: 0 !important;
+    }
+  }
+
+  .link-expired {
+    font-size: 18px;
+  }
+}
+
+.active-bgc {
+  background: initial;
+  background-color: #fff !important;
 }
 </style>
