94f406da1915a28341ffd67adad73abb78c7ef91
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 13:55:10 +0800

    迁移：新增 ERP 与 WMS 系统切换入口

---
+++ src/layout/components/NavWMS.vue
@@ -0,0 +1,73 @@
+<template>
+  <div style="vertical-align: top">
+    <div v-if="hasWmsPermission && wmsUrl" @click="navToWms">
+      <div class="my-nav-btn">
+        <el-icon><Menu /></el-icon>
+        {{ $t('ui.wmsWarehouse') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Menu } from '@element-plus/icons-vue'
+import { queryDomainName, exchange } from '@/api/login'
+import { getToken } from '@/utils/auth'
+import { checkPermi } from '@/utils/permission'
+
+export default {
+  name: 'NavWMS',
+  components: {
+    Menu
+  },
+  data() {
+    return {
+      wmsUrl: ''
+    }
+  },
+  computed: {
+    hasWmsPermission() {
+      return checkPermi(['wms:system'])
+    }
+  },
+  created() {
+    this.queryDomainName()
+  },
+  methods: {
+    queryDomainName() {
+      queryDomainName().then(res => {
+        this.wmsUrl = res.data.wmsDomain
+      })
+    },
+    navToWms() {
+      exchange({ token: getToken(), tenantType: 2 }).then(res => {
+        const data = res.data || {}
+        if (data.access_token) {
+          const url = `${this.wmsUrl}/authLogin?erpToken=${data.access_token}`
+          window.open(url, '_self')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-nav-btn {
+  cursor: pointer;
+  border: 1px solid #409eff;
+  padding: 0 10px;
+  border-radius: 4px;
+  color: #409eff;
+  font-size: 12px;
+  height: 32px;
+  line-height: 32px;
+  margin-top: 9px;
+  transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+
+  .el-icon {
+    vertical-align: -2px;
+    margin-right: 2px;
+  }
+}
+</style>
