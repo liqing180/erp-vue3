5306e357ea653b03d9e0931ca7a733b740d9e130
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 11:00:31 +0800

    refactor: register ERP Vue3 directives

--- src/directive/index.js
+++ src/directive/index.js
@@ -2,15 +2,18 @@ import hasRole from './permission/hasRole'
 import hasPermi from './permission/hasPermi'
 import copyText from './common/copyText'
 import dialogDragWidth from './dialog/dragWidth'
+import dialogDragHeight from './dialog/dragHeight'
 import thousandSplit from './thousandSplit'
 import dragTree from './dragTree'
+import tableTab from './table-tab'
+
 export default function directive(app) {
   app.directive('hasRole', hasRole)
   app.directive('hasPermi', hasPermi)
   app.directive('copyText', copyText)
   app.directive('dialogDragWidth', dialogDragWidth)
-  /* 数字千分符 */
+  app.directive('dialogDragHeight', dialogDragHeight)
   app.directive('thousandSplit', thousandSplit)
-  /* 拖拽树 */
   app.directive('dragTree', dragTree)
+  app.directive('table-tab', tableTab)
 }
