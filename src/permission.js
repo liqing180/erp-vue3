19750e6864cee3ff5ea11be92fec866d1cc4cf37
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 13:54:53 +0800

    迁移：对齐 ERP 登录权限跳转规则

--- src/permission.js
+++ src/permission.js
@@ -20,60 +20,53 @@ const whiteList = [
   '/appH5/verification/verification',
   '/appH5/verification/signature',
   '/settingNewPassword',
-  '/customerQuestionnaire'
+  '/customerQuestionnaire',
+  '/authLogin'
 ]
 
 router.beforeEach((to, from, next) => {
   router.prevRoute = from
   NProgress.start()
+
   if (whiteList.indexOf(to.path) !== -1) {
-    // 在免登录白名单，直接进入
     next()
   } else if (getToken()) {
     to.meta.title && store.dispatch('settings/setTitle', to.meta.title)
-    /* has token */
+
     if (to.path === '/login') {
       next({ path: '/' })
       NProgress.done()
+    } else if (store.getters.roles.length === 0) {
+      store
+        .dispatch('GetInfo')
+        .then(() => {
+          store.dispatch('GenerateRoutes').then(accessRoutes => {
+            accessRoutes.forEach(item => {
+              router.addRoute(item)
+            })
+            next({ ...to, replace: true })
+          })
+        })
+        .catch(err => {
+          store.dispatch('LogOut').then(() => {
+            ElMessage.error(err)
+            next({ path: '/' })
+          })
+        })
     } else {
-      if (store.getters.roles.length === 0) {
-        // 判断当前用户是否已拉取完user_info信息
-        store
-          .dispatch('GetInfo')
-          .then(() => {
-            store.dispatch('GenerateRoutes').then(accessRoutes => {
-              // 根据roles权限生成可访问的路由表
-              accessRoutes.forEach(item => {
-                router.addRoute(item) // 动态添加可访问路由表
-              })
-
-              next({ ...to, replace: true }) // hack方法 确保addRoutes已完成
-            })
-          })
-          .catch(err => {
-            store.dispatch('LogOut').then(() => {
-              ElMessage.error(err)
-              next({ path: '/' })
-            })
-          })
-      } else {
-        next()
-      }
-    }
-  } else {
-    // 没有token
-    if (whiteList.indexOf(to.path) !== -1) {
-      // 在免登录白名单，直接进入
       next()
-    } else {
-      const type = sessionStorage.getItem('type') || ''
-      if (type && type !== 'undefined') {
-        next('/login?redirect=/index&type=' + type) // 否则全部重定向到登录页
-      } else {
-        next('/login?redirect=/index') // 否则全部重定向到登录页
-      }
-      NProgress.done()
     }
+  } else if (to.path === '/bpm/history') {
+    next(`/login?redirect=${encodeURIComponent(to.fullPath)}`)
+    NProgress.done()
+  } else {
+    const type = sessionStorage.getItem('type') || ''
+    if (type && type !== 'undefined') {
+      next('/login?redirect=/index&type=' + type)
+    } else {
+      next('/login?redirect=/index')
+    }
+    NProgress.done()
   }
 })
 
