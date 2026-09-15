b6b559ef846804a927e23c58c7176b7606cc8869
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 11:18:48 +0800

    迁移：补充表格必填错误提示组件

---
+++ src/components/Common/TablePropError/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="table-prop-error">
+    {{ $t('ui.reqMsgForTableRow') }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TablePropError'
+}
+</script>
+
+<style scoped>
+.table-prop-error {
+  padding: 3px 0;
+  color: #f56c6c;
+  font-size: 12px;
+  line-height: 13px;
+  text-align: left;
+}
+</style>
