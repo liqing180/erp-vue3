c9407fec3769c5729eb552b047625523ef969899
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 14:42:23 +0800

    迁移：首页仪表盘国际化

---
+++ src/views/reportManagement/dashboard/lang/en.js
@@ -0,0 +1,30 @@
+export default {
+  DASHBOARD: {
+    noAuthMsg:
+      'Your account has not been assigned with access to a dashboard yet. Please contact your system administrator.',
+    taskCenter: 'Task Center',
+    todayTransactions: "Today's Transactions",
+    open: 'Open',
+    overdue: 'Overdue',
+    processed: 'Processed',
+    myInitiated: 'My Initiated',
+    notifications: 'Notifications',
+    todaysOrders: "Today's Orders",
+    todaysStockIns: "Today's Stock-Ins",
+    todaysReturns: "Today's Returns",
+    activeSuppliers: 'Active Suppliers',
+    todaysDeliveries: "Today's Deliveries",
+    activeCustomers: 'Active Customers',
+    purchaseOrders: 'Purchase Orders',
+    salesOrders: 'Sales Orders',
+    reportSwitching: 'Report Switching',
+    purchase: 'Purchase',
+    sales: 'Sales',
+    overall: 'Overall',
+    group: 'Group',
+    reportGroup: 'Report Group',
+    report: 'Report',
+    role: 'Role',
+    system: 'System'
+  }
+}
