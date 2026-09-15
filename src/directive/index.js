337689452ef522fb2918d5546831e37c912b8016
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 11:56:20 +0800

    迁移：统一注册 ERP 公共指令

--- src/directive/index.js
+++ src/directive/index.js
@@ -6,7 +6,11 @@ import dialogDragHeight from './dialog/dragHeight'
 import thousandSplit from './thousandSplit'
 import tenRowsHeight from './tenRowsHeight'
 import dragTree from './dragTree'
+import dragHistory from './dragHistory'
 import tableTab from './table-tab'
+import defaultSelect from './tagCloseNone'
+import optionsTitleFmt from './optionsTitleFmt'
+import setCascaderLabelTitle from './setCascaderLabelTitle'
 
 export default function directive(app) {
   app.directive('hasRole', hasRole)
@@ -17,5 +21,9 @@ export default function directive(app) {
   app.directive('thousandSplit', thousandSplit)
   app.directive('tenRowsHeight', tenRowsHeight)
   app.directive('dragTree', dragTree)
+  app.directive('dragHistory', dragHistory)
   app.directive('table-tab', tableTab)
+  app.directive('defaultSelect', defaultSelect)
+  app.directive('optionsTitleFmt', optionsTitleFmt)
+  app.directive('setCascaderLabelTitle', setCascaderLabelTitle)
 }
