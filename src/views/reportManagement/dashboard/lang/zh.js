b65f3ab5b7e46de0150cb7747043d098033b8397
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 14:42:31 +0800

    迁移：首页仪表盘国际化

---
+++ src/views/reportManagement/dashboard/lang/zh.js
@@ -0,0 +1,29 @@
+export default {
+  DASHBOARD: {
+    noAuthMsg: '您的账户尚未被授予仪表板的访问权限。请联系您的系统管理员。',
+    taskCenter: '任务中心',
+    todayTransactions: '今日数据',
+    open: '待处理',
+    overdue: '已逾期',
+    processed: '已处理',
+    myInitiated: '我发起的',
+    notifications: '知会我的',
+    todaysOrders: '今日订单数',
+    todaysStockIns: '今日入库',
+    todaysReturns: '今日退货',
+    activeSuppliers: '活跃供应商',
+    todaysDeliveries: '今日出库',
+    activeCustomers: '活跃客户',
+    purchaseOrders: '采购订单数',
+    salesOrders: '销售订单数',
+    reportSwitching: '报表切换',
+    purchase: '采购',
+    sales: '销售',
+    overall: '全局',
+    group: '文件夹',
+    reportGroup: '文件夹名称',
+    report: '报表',
+    role: '角色',
+    system: '系统'
+  }
+}
