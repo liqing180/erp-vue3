0682ef6453e4e03cb316e1d288df0c23f97dec5a
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 11:01:07 +0800

    feat: add ERP menu permission keys

---
+++ src/config/menuKey.js
@@ -0,0 +1,36 @@
+export default {
+  BP: 'businessPartner:businessPartner',
+  productInfo: 'productManagement:productInfo',
+
+  // 采购
+  PR: 'purchaseManagement:purchaseRequisition',
+  RFQ: 'purchaseManagement:requestForQuotation',
+  PQ: 'purchaseManagement:purchaseQuotation',
+  PO: 'purchaseManagement:purchaseOrder',
+  purchaseReturnOrder: 'purchaseManagement:purchaseReturnOrder',
+  consignmentOrder: 'purchaseManagement:consignmentOrder',
+  logisticsCost: 'purchaseManagement:logisticsCost',
+  supplierContract: 'purchaseManagement:supplierContract',
+  ASL: 'purchaseManagement:asl',
+
+  // 销售
+  customerQuestionnaire: 'salesManagement:customerQuestionnaire',
+  serviceOrder: 'salesManagement:serviceOrder',
+  salesInquiry: 'salesManagement:salesInquiry',
+  salesQuotation: 'salesManagement:salesQuotation',
+  preDeliveryNotice: 'salesManagement:preDeliveryNotice',
+  salesGoodsReturn: 'salesManagement:salesGoodsReturn',
+  proformaInvoice: 'salesManagement:proformaInvoice',
+
+  // 项目 / 库存 / 产品
+  project: 'projectManagement:project',
+  transferOrder: 'inventoryManagement:transferOrder',
+  inventoryAudit: 'inventoryManagement:inventoryAudit',
+  inventoryReport: 'inventoryManagement:inventoryReport',
+  safetyStock: 'inventoryManagement:safetyStock',
+  inventoryInitialization: 'inventoryManagement:inventoryInitialization',
+  storeIssueChit: 'projectManagement:storeIssueChit',
+  projectProduction: 'projectManagement:projectProduction',
+  productRelationship: 'productManagement:productRelationship',
+  emergencyGoodsReceipt: 'inventoryManagement:emergencyGoodsReceipt'
+}
