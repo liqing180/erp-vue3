acb02b50314ac3622c26cc04aa1ecfefceeef403
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 14:43:08 +0800

    迁移：首页报表 iframe 动态高度

--- src/components/iFrame/index.vue
+++ src/components/iFrame/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-loading="loading" :style="'height:' + height">
+  <div v-loading="loading" :style="{ height }">
     <iframe
       :src="url"
       frameborder="no"
@@ -10,23 +10,44 @@
 </template>
 
 <script setup>
+import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+
 const props = defineProps({
   src: {
     type: String,
     required: true
+  },
+  otherHeight: {
+    type: Number,
+    default: 94.5
   }
 })
 
-const height = ref(document.documentElement.clientHeight - 94.5 + 'px;')
+const height = ref('0px')
 const loading = ref(true)
 const url = computed(() => props.src)
+let loadingTimer
+
+function updateHeight() {
+  const availableHeight =
+    document.documentElement.clientHeight - (props.otherHeight || 94.5)
+  height.value = `${Math.max(0, availableHeight)}px`
+}
+
+watch(() => props.otherHeight, updateHeight)
 
 onMounted(() => {
-  setTimeout(() => {
+  updateHeight()
+  window.addEventListener('resize', updateHeight)
+  loadingTimer = window.setTimeout(() => {
     loading.value = false
   }, 300)
-  window.onresize = function temp() {
-    height.value = document.documentElement.clientHeight - 94.5 + 'px;'
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateHeight)
+  if (loadingTimer) {
+    window.clearTimeout(loadingTimer)
   }
 })
 </script>
