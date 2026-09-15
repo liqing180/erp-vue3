cd18988a6c4f3144367e9421bab79e1f93de19ea
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-15 14:31:51 +0800

    迁移：首页仪表盘多语言

---
+++ src/lang/dashboard.js
@@ -0,0 +1,61 @@
+export default {
+  zh: {
+    DASHBOARD: {
+      noAuthMsg: '您的账户尚未被授予仪表板的访问权限。请联系您的系统管理员.',
+      taskCenter: '任务中心',
+      todayTransactions: '今日数据',
+      open: '待处理',
+      overdue: '已逾期',
+      processed: '已处理',
+      myInitiated: '我发起的',
+      notifications: '知会我的',
+      todaysOrders: '今日订单数',
+      todaysStockIns: '今日入库',
+      todaysReturns: '今日退货',
+      activeSuppliers: '活跃供应商',
+      todaysDeliveries: '今日出库',
+      activeCustomers: '活跃客户',
+      purchaseOrders: '采购订单数',
+      salesOrders: '销售订单数',
+      reportSwitching: '报表切换',
+      purchase: '采购',
+      sales: '销售',
+      overall: '全局',
+      group: '文件夹',
+      reportGroup: '文件夹名称',
+      report: '报表',
+      role: '角色',
+      system: '系统'
+    }
+  },
+  en: {
+    DASHBOARD: {
+      noAuthMsg:
+        'Your account has not been assigned with access to a dashboard yet. Please contact your system administrator.',
+      taskCenter: 'Task Center',
+      todayTransactions: "Today's Transactions",
+      open: 'Open',
+      overdue: 'Overdue',
+      processed: 'Processed',
+      myInitiated: 'My Initiated',
+      notifications: 'Notifications',
+      todaysOrders: "Today's Orders",
+      todaysStockIns: "Today's Stock-Ins",
+      todaysReturns: "Today's Returns",
+      activeSuppliers: 'Active Suppliers',
+      todaysDeliveries: "Today's Deliveries",
+      activeCustomers: 'Active Customers',
+      purchaseOrders: 'Purchase Orders',
+      salesOrders: 'Sales Orders',
+      reportSwitching: 'Report Switching',
+      purchase: 'Purchase',
+      sales: 'Sales',
+      overall: 'Overall',
+      group: 'Group',
+      reportGroup: 'Report Group',
+      report: 'Report',
+      role: 'Role',
+      system: 'System'
+    }
+  }
+}
