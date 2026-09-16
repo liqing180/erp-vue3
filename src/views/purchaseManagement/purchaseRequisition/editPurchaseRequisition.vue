3c5280def5e5b33be5b98d51e8aca7d436c30fd4
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-16 11:01:32 +0800

    feat(pr): add PR edit entry

---
+++ src/views/purchaseManagement/purchaseRequisition/editPurchaseRequisition.vue
@@ -0,0 +1,16 @@
+<template>
+  <PurchaseRequisitionForm
+    :key="$route.query.timeId || $route.query.id"
+    mode="edit"
+    :id="$route.query.id || ''"
+  />
+</template>
+
+<script>
+import PurchaseRequisitionForm from './PurchaseRequisitionForm.vue'
+
+export default {
+  name: 'EditPurchaseRequisition',
+  components: { PurchaseRequisitionForm }
+}
+</script>
