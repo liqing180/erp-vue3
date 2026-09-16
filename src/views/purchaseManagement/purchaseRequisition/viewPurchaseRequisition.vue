6f0ab194d3c9e0712ef88d68ca5d9d377c11c159
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-16 11:01:44 +0800

    feat(pr): add PR view entry

---
+++ src/views/purchaseManagement/purchaseRequisition/viewPurchaseRequisition.vue
@@ -0,0 +1,16 @@
+<template>
+  <PurchaseRequisitionForm
+    :key="$route.query.timeId || $route.query.id"
+    mode="view"
+    :id="$route.query.id || ''"
+  />
+</template>
+
+<script>
+import PurchaseRequisitionForm from './PurchaseRequisitionForm.vue'
+
+export default {
+  name: 'ViewPurchaseRequisition',
+  components: { PurchaseRequisitionForm }
+}
+</script>
