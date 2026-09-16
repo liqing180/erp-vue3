d51803a7d8c911e2f360d262dd2731e948e28df0
liqing180 <54855614+liqing180@users.noreply.github.com>
2026-09-16 11:01:38 +0800

    feat(pr): add PR revision entry

---
+++ src/views/purchaseManagement/purchaseRequisition/revisePurchaseRequisition.vue
@@ -0,0 +1,16 @@
+<template>
+  <PurchaseRequisitionForm
+    :key="$route.query.timeId || $route.query.id"
+    mode="revise"
+    :id="$route.query.id || ''"
+  />
+</template>
+
+<script>
+import PurchaseRequisitionForm from './PurchaseRequisitionForm.vue'
+
+export default {
+  name: 'RevisePurchaseRequisition',
+  components: { PurchaseRequisitionForm }
+}
+</script>
